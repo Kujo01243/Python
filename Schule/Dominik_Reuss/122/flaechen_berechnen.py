ef099dd1820e5849de967667b5d82d2267ca149f
Josias Theil <91317627+Kujo01243@users.noreply.github.com>
2022-11-23 12:39:22 +0100

    ff

--- Schule/Dominik_Reuss/122/flaechen_berechnen.py
+++ Schule/Dominik_Reuss/122/flaechen_berechnen.py
@@ -1,11 +1,10 @@
 pi = 3.142
 
-
 def kreis():
     durchmesser1=100
     durchmesser2=10
-    grokre = durchmesser1/2*durchmesser1/2*pi
-    kleikre = durchmesser2/2*durchmesser2/2*pi
+    grokre = ((durchmesser1/2)**2/2*pi)
+    kleikre = ((durchmesser2/2)**2/2*pi)
     result = (grokre-kleikre)
     return result
 print("Resultat: ", kreis(), "mm2")
@@ -19,5 +18,4 @@ def dreieck():
     result = (flächedreieck-quadratfläche)
     return result
 print("Resultat: ", dreieck(), "mm2")
-
 
